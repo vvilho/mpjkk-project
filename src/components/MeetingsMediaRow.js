e2f5ee5ef85c569dbea0d888c819f57fd2b80658
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-05-04 17:46:49 +0300

    meetup row design fix

--- src/components/MeetingsMediaRow.js
+++ src/components/MeetingsMediaRow.js
@@ -3,23 +3,21 @@
 import React, {useEffect, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Moment from 'react-moment';
-
-import {Paper, Avatar, CardContent, CardHeader, Card, Grid, Button, IconButton} from '@material-ui/core';
+import {Paper, Avatar, CardContent, CardHeader, Card, Grid, Button, IconButton, CardActions} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 // import FavoriteIcon from '@material-ui/icons/Favorite';
 import Box from '@material-ui/core/Box';
-
-
+import CardMedia from '@material-ui/core/CardMedia';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
 import {useTag} from '../hooks/ApiHooks';
-
 // import {Grid, GridListTileBar, IconButton, makeStyles} from '@material-ui/core';
 import {Link as RouterLink, withRouter} from 'react-router-dom';
 import RoomIcon from '@material-ui/icons/Room';
 import dateFormat from 'dateformat';
 import DeleteIcon from '@material-ui/icons/Delete';
 import moment from 'moment';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,6 +26,9 @@ const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
+    marginLeft: '-1em',
+    marginRight: '-1em',
+    marginTop: '-1em',
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -70,6 +71,7 @@ const useStyles = makeStyles((theme) => ({
 const MeetingsMediaRow = ({file, ownFiles, history, deleteMedia}) => {
   const classes = useStyles();
   const [avatar, setAvatar] = useState();
+  const screenNarrow = useMediaQuery('(max-width:600px)');
 
   const {getTag} = useTag();
 
@@ -90,13 +92,30 @@ const MeetingsMediaRow = ({file, ownFiles, history, deleteMedia}) => {
 
   const meetingHappened = () => {
     if (desc.time_end < (moment().format()).slice(0, -9)) {
-      return 'Happened';
+      return (
+        <Typography
+          variant="subtitle2"
+          style={{fontSize: '0.8em'}}>
+          Happened
+        </Typography>
+      );
     } else if (desc.time_start < (moment().format()).slice(0, -9) && desc.time_end > (moment().format()).slice(0, -9)) {
-      return 'Happening right now';
+      return (
+        <Typography
+          variant="subtitle2"
+          style={{fontSize: '0.8em'}}>
+          Happening right now
+        </Typography>
+      );
     } else {
       return <>
-        <Typography>Happening in</Typography>
-        <Moment fromNow ago>{desc.time_start}</Moment></>;
+        <Typography
+          variant="subtitle2"
+          style={{fontSize: '0.8em'}}>
+            Happening in&nbsp;
+          <Moment fromNow ago>{desc.time_start}</Moment>
+        </Typography>
+      </>;
     }
   };
 
@@ -112,17 +131,17 @@ const MeetingsMediaRow = ({file, ownFiles, history, deleteMedia}) => {
   return (
     <>
 
-
-      <Card
-        variant="outlined"
-        classes={classes.card}
-        style={{
-          height: '500px',
-        }}
-
+      <Grid
+        container
+        justify={'center'}
       >
-        <Box>
-          {ownFiles &&
+        <Grid item xs={screenNarrow? 12 : 8}>
+          <Card
+            variant="outlined"
+            classes={classes.card}
+          >
+            <Box>
+              {ownFiles &&
         <Box display="flex" justifyContent="flex-end" className={classes.top}>
           <IconButton
             aria-label={`delete file`}
@@ -141,125 +160,142 @@ const MeetingsMediaRow = ({file, ownFiles, history, deleteMedia}) => {
             <DeleteIcon/>
           </IconButton>
         </Box>
-          }
-        </Box>
-        <Box
-          display="flex"
-        >
-          <Box>
-            <CardHeader
-              avatar={
-                <Avatar
-                  variant={'circle'}
-                  src={avatar}
-                  aria-label="avatar"
-                  className={classes.avatar}
-                >
-
-                </Avatar>
               }
-            />
-          </Box>
-          <Box className={classes.paddingBox}>
-            <Typography gutterBottom variant="h6" component="h2" className={classes.lines}>
-              {file.title}
-            </Typography>
-            <Typography>{desc.owner}</Typography>
-          </Box>
-        </Box>
-        <Paper
-          style={{
-            padding: '15px',
-            margin: '15px',
-            height: '335px'
-            ,
-          }}
-          elevation={5}
-        >
-          <Moment
-            format={'DD/MM/YY'}
-            date={desc.time_start}
-          />
-          <Typography
-            variant="h6"
-
-          >{dateFormat(desc.time_start, 'HH:MM')}</Typography>
-          <Typography>{meetingHappened()}</Typography>
-
-
-          <Grid
-            container
-            direction={'column'}
-            alignItems={'center'}
-            style={{
-              height: '60%',
-            }}
-          >
-            <Grid
-              item
+            </Box>
+            <Box
+              display="flex"
             >
-              <RoomIcon />
-            </Grid>
-            <Grid
-              item
-            >
-              <Typography
-                gutterBottom
-                variant="h6"
+              <Box>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      variant={'circle'}
+                      src={avatar}
+                      aria-label="avatar"
+                      className={classes.avatar}
+                    >
 
-                component="h6"
-              >{desc.zipcode}</Typography>
-            </Grid>
-            <Grid
-              item
+                    </Avatar>
+                  }
+                />
+              </Box>
+              <Box className={classes.paddingBox}>
+                <Typography gutterBottom variant="h5" component="h2" className={classes.lines}>
+                  {file.title}
+                </Typography>
+                <Typography
+                  className={classes.lines}
+                  style={{fontSize: '0.8em'}}
+                >
+                  {desc.owner}
+                </Typography>
+              </Box>
+            </Box>
+            <Paper
+              style={{
+                padding: '15px',
+                margin: '0 3em',
+                marginTop: '0.5em',
+                height: 'auto',
+              }}
+              elevation={5}
             >
-              <Typography
-                gutterBottom
-                variant="h6"
+              <Grid
 
-                component="h6"
-              >{desc.city}</Typography>
-            </Grid>
-            <Grid
-              item
-            >
-              <Typography
-                variant="body2"
-                color="textSecondary"
-                style={{wordWrap: 'break-word'}}
               >
-                {desc.description.length > 300 ?
+                <CardMedia
+                  className={classes.media}
+                  image={uploadsUrl + file.filename}
+                  alt={file.title}
+                />
+              </Grid>
+              <Typography
+                variant="subtitle2"
+                style={{fontSize: '1em', paddingTop: '0.5em'}}>
+                <Moment
+                  format={'dddd, MMMM D, yyyy'}
+                  date={desc.time_start}
+                />
+              </Typography>
+              <Typography
+                variant="subtitle2"
+                style={{fontSize: '1em'}}
+              >
+                From {dateFormat(desc.time_start, 'HH:MM')}
+              </Typography>
+              {meetingHappened()}
+              <Grid
+                container
+                direction={'column'}
+                alignItems={'center'}
+                style={{
+                  height: '60%',
+                }}
+              >
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  style={{padding: '1em'}}>
+                  <Grid
+                    item
+                  >
+                    <RoomIcon />
+                  </Grid>
+                  <Grid
+                    item
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+
+                      component="h6"
+                    >{desc.city}</Typography>
+                  </Grid>
+                </Box>
+                <Grid
+                  item
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    style={{wordWrap: 'break-word', fontSize: '1.1em', marginBottom: '1em'}}
+                  >
+                    {desc.description.length > 300 ?
                 desc.description.slice(0, 300) + '...' :
                 desc.description}
-              </Typography>
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                justify={'center'}
+              >
+                <CardActions>
+                  <Button
+                    aria-label={`info about ${file.title}`}
+                    component={RouterLink}
+                    to={{
+                      pathname: '/meetingssingle',
+                      state: file,
 
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            justify={'center'}
-          >
-            <Button
-              aria-label={`info about ${file.title}`}
-              component={RouterLink}
-              to={{
-                pathname: '/meetingssingle',
-                state: file,
-
-              }}
-              className={classes.icon}
-              color="secondary"
-            >
+                    }}
+                    className={classes.icon}
+                    color="secondary"
+                  >
             Read more
-            </Button>
-          </Grid>
-        </Paper>
+                  </Button>
+                </CardActions>
+              </Grid>
+            </Paper>
 
-        <CardContent>
+            <CardContent>
 
-        </CardContent>
+            </CardContent>
 
-      </Card>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 };
