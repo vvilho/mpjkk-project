1360bfd3040044c84b05a895fef9247e8c91e14d
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-05-05 11:33:28 +0300

    slogan text

--- src/views/Fundings.js
+++ src/views/Fundings.js
@@ -8,23 +8,36 @@ const Fundings = () => {
     <>
       <Grid>
         <Grid item
-          style={{
-            margin: '2em 0',
-          }}>
-          <Typography
-            component="body1"
-            variant="h5"
-            // eslint-disable-next-line max-len
-            gutterBottom>Welcome to the community of people interested in environmental issues!
-          </Typography>
-        </Grid>
-        <Grid item
         >
           <Typography
             component="h1"
             variant="h2"
             color="primary"
             gutterBottom>Fundings</Typography>
+        </Grid>
+        <Grid item
+          style={{
+            margin: '2em 0',
+          }}>
+          <Typography
+            component="body1"
+            variant="h2"
+            style={{fontSize: '1.8em'}}
+            // eslint-disable-next-line max-len
+            gutterBottom>Welcome to the community of people interested in environmental issues!
+          </Typography>
+        </Grid>
+        <Grid item
+          style={{
+            margin: '2em 0',
+          }}>
+          <Typography
+            component="body1"
+            variant="h2"
+            style={{fontSize: '1.2em'}}
+            // eslint-disable-next-line max-len
+            gutterBottom>&#8226;&nbsp;Create funding projects to solve environmental issues!&nbsp;&#8226;&nbsp;Donate money to make these projects come true!
+          </Typography>
         </Grid>
       </Grid>
       <FundingsMediaTable/>
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,25 +5,35 @@ import {Typography, Grid} from '@material-ui/core';
 const Home = () => {
   return (
     <>
-      <Grid>
-        <Grid item
-          style={{
-            margin: '2em 0',
-          }}>
-          <Typography
-            component="body1"
-            variant="h5"
-            // eslint-disable-next-line max-len
-            gutterBottom>Welcome to the community of people interested in environmental issues!
-          </Typography>
-        </Grid>
+      <Grid container>
         <Grid item
         >
           <Typography
             component="h1"
             variant="h2"
             color="primary"
-            gutterBottom>Blog</Typography>
+            style={{display: 'flex'}}
+            gutterBottom>Blog
+          </Typography>
+          <Typography
+            component="body1"
+            variant="h2"
+            style={{fontSize: '1.8em'}}
+            // eslint-disable-next-line max-len
+            gutterBottom>Welcome to the community of people interested in environmental issues!
+          </Typography>
+        </Grid>
+        <Grid item
+          style={{
+            margin: '2em 0',
+          }}>
+          <Typography
+            component="body1"
+            variant="h2"
+            style={{fontSize: '1.2em'}}
+            // eslint-disable-next-line max-len
+            gutterBottom>&#8226;&nbsp;Share your thoughts about environmental issues!&nbsp;&#8226;&nbsp;Show your environmental clever art!&nbsp;&#8226;&nbsp;Show how the funding project turned out!
+          </Typography>
         </Grid>
       </Grid>
       <BlogMediaTable ownFiles={false}/>
